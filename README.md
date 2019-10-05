a267721acd743c20cdf5b783a44607cdc844dd7a
Remo H. Jansen <remo.jansen@wolksoftware.com>
2019-10-05 17:16:00 +0100

    Support to filter by date

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Transforms Allied Irish Banks (AIB) a bank statement (.csv) into a Xero bank fee
 
 2. Export your AIB bank feed to .csv
 
-3. Convert the AIB csv into a Xero csv with `aib-to-xero input_file_name.csv output_file_name.csv`
+3. Convert the AIB csv into a Xero csv with:
 
-4. Upload the output file to the Xero bank fee import tool.
+   ```sh
+   aib-to-xero input_file_name.csv output_file_name.csv
+   ```
+
+   It is also possible to ignore all entries prior to a date using the `--since` or `-s` flag:
+
+   ```sh
+   aib-to-xero -s 30/09/19 input_file_name.csv output_file_name.csv
+   ```
+
+4. Upload the output file to the Xero bank feed import tool.
--- index.js
+++ index.js
@@ -1,39 +1,83 @@
 #!/usr/bin/env node
 
+const arg = require("arg");
 const csv = require("csv-parser");
 const fs = require("fs");
+const { promisify } = require("util");
+const moment = require("moment");
 
-function parseArgs(args) {
-  const inputFile = args[2];
-  const outputFile = args[3];
+/**
+ * @param {string[]} rawArgs The command line raw arguments
+ * @returns {{ since: string|undefined, inputFile: string, outputFile: string}}
+ */
+function parseArgs(rawArgs) {
+  const options = {
+    since: "--since"
+  };
+  const aliases = {
+    [options.since]: "-s"
+  };
+  const args = arg(
+    {
+      [options.since]: String,
+      [aliases[options.since]]: options.since
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  );
+  /** @type {string|undefined} */
+  const since = args[options.since];
+  const inputFile = args["_"][0];
+  const outputFile = args["_"][1];
   return {
+    since,
     inputFile,
     outputFile
   };
 }
 
-function parseCsv(filePath, cb) {
-  const output = [];
-  fs.createReadStream(filePath)
-    .pipe(csv())
-    .on("data", row => {
-      const keys = Object.keys(row);
-      const cleandRow = keys.reduce((prev, curr) => {
-        return {
-          ...prev,
-          ...{
-            [curr.toString().trim()]: row[[curr]]
-          }
-        };
-      }, {});
-      output.push(cleandRow);
-    })
-    .on("end", () => {
-      cb(output);
-    });
+/**
+ * @param {string} [filePath] - The csv file path
+ * @returns {Promise<{}[]>}
+ */
+async function parseCsv(filePath) {
+  return new Promise((resolve, reject) => {
+    /** @type {{}[]}*/
+    const output = [];
+    if (filePath) {
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on("data", row => {
+          const keys = Object.keys(row);
+          const cleandRow = keys.reduce((prev, curr) => {
+            return {
+              ...prev,
+              ...{
+                [curr.toString().trim()]: row[curr]
+              }
+            };
+          }, {});
+          output.push(cleandRow);
+        })
+        .on("error", (/** @type {Error} */ err) => {
+          reject(err);
+        })
+        .on("end", () => {
+          resolve(output);
+        });
+    } else {
+      reject(new Error("Missing input path!"));
+    }
+  });
 }
 
-function generateCsv(output) {
+/**
+ * @param {any[]} inputContent The content of the input csv file
+ * @param {string|undefined} since The date to be used as the start of the feed
+ * @returns {string}
+ */
+function generateCsv(inputContent, since) {
   const outputHeaders = [
     "*Date",
     "*Amount",
@@ -51,7 +95,8 @@ function generateCsv(output) {
     "Balance",
     "Transaction Type"
   ];
-  const data = output
+  const dateFormat = "DD/MM/YY";
+  const data = inputContent
     .map(row => {
       const date = row[inputHeaders[1]];
       const description = row[inputHeaders[2]];
@@ -67,21 +112,65 @@ function generateCsv(output) {
           return null;
       }
     })
-    .filter(row => row !== null);
-
-  return outputHeaders.join(",") + "\n" + data.map(d => d.join(",")).join("\n");
-}
-
-function main() {
-  const args = parseArgs(process.argv);
-  parseCsv(args.inputFile, output => {
-    const outputCsv = generateCsv(output);
-    fs.writeFile(args.outputFile, outputCsv, function(err) {
-      if (!err) {
-        console.log("Success!");
+    .filter(row => row !== null)
+    .map(r => {
+      if (r) {
+        const date = moment(r[0], dateFormat);
+        const amount = r[1];
+        const payee = r[2];
+        const description = r[3];
+        return [date, amount, payee, description];
       }
-    });
-  });
+    })
+    .filter(r => {
+      if (since && r) {
+        const sinceDate = moment(since, dateFormat);
+        const date = r[0];
+        return sinceDate.isBefore(date);
+      } else {
+        return true;
+      }
+    })
+    .map(r => (r ? [r[0].format(dateFormat), r[1], r[2], r[3]] : null));
+  return (
+    outputHeaders.join(",") +
+    "\n" +
+    data.map(d => (d ? d.join(",") : "")).join("\n")
+  );
 }
 
-main();
+/**
+ * @param {string} [path] The path of the output file
+ * @param {string} [outputCsv] The csv content
+ * @returns {Promise<void>}
+ */
+async function save(path, outputCsv) {
+  if (path) {
+    const writeFileAsync = promisify(fs.writeFile);
+    try {
+      return await writeFileAsync(path, outputCsv);
+      console.log("Success!");
+    } catch (err) {
+      return Promise.reject(new Error("Missing output path!"));
+    }
+  } else {
+    return Promise.reject(new Error("Missing output path!"));
+  }
+}
+
+/**
+ * @returns {Promise<void>}
+ */
+async function main() {
+  try {
+    const args = parseArgs(process.argv);
+    const inputContent = await parseCsv(args.inputFile);
+    const outputCsv = generateCsv(inputContent, args.since);
+    await save(args.outputFile, outputCsv);
+    console.log("Success!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+(async () => await main())();
